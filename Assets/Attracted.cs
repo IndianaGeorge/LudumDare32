fe92d5da1cd2080a9b2b2b9722c7642f373ce37a
Jorge Fuente-Alba <wadster@gmail.com>
2015-04-20 20:23:46 -0400

    Lifting rocks

---
+++ Assets/Attracted.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class Attracted : MonoBehaviour {
+	public string attractorTag;
+	public float range;
+	public float intensity;
+	public float maxRotation;
+	Rigidbody rigidBody;
+	float rotationSpeed = 0;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float sqRange = range * range;
+		Vector3 diff;
+		Vector3 position = transform.position;
+		GameObject[] attractors;
+		GameObject closestAttractor;
+		float minSqrDistance = range*range;
+		attractors = GameObject.FindGameObjectsWithTag (attractorTag);
+		if (attractors.Length > 0) {
+			diff = attractors[0].transform.position - position;
+			minSqrDistance = diff.sqrMagnitude;
+			closestAttractor = attractors[0];
+			foreach (GameObject attractor in attractors) {
+				diff = attractor.transform.position - position;
+				if (diff.sqrMagnitude < minSqrDistance) {
+					closestAttractor = attractor;
+					minSqrDistance = diff.sqrMagnitude;
+				}
+			}
+			if (minSqrDistance < sqRange) {
+				if (!rigidBody.isKinematic) {
+					rigidBody.isKinematic = true;
+					rigidBody.detectCollisions = false;
+					rotationSpeed = Random.value * maxRotation;
+				}
+				diff = closestAttractor.transform.position - position;
+				Debug.Log (((sqRange/diff.sqrMagnitude)).ToString());
+				float strength = (1-(diff.sqrMagnitude/sqRange));
+				transform.localPosition = transform.localPosition + (diff*strength*Time.deltaTime);
+				transform.Rotate(0,0,rotationSpeed*Time.deltaTime);
+			}
+			else { // back to physics
+				if (rigidBody.isKinematic) {
+					rigidBody.isKinematic = false;
+					rigidBody.detectCollisions = true;
+				}
+			}
+		}
+	}
+}
